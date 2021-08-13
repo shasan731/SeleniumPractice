6e160a3c589aa37a6d340b3fc7b0b20cb3c4a2a9
shahriar <shasan731@gmail.com>
2021-08-13 20:48:22 +0600

    firt commit for Selenium Practice

---
+++ UiTests/SeleniumShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UiTests
+{
+    public class SeleniumShould
+    {
+        [Fact]
+        [Trait ("Category","Smoke")]
+        public void LoadPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.google.com/");
+            }
+
+        }
+    }
+}
